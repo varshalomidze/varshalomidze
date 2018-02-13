0d7b033693cb14bbfb7eaa3e37ce702c6744b6e2
Enver Varshalomidze <Enri@en.local>
2018-02-13 23:49:34 +0400

    Initial Commit

---
+++ varshalomidze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  varshalomidze
+//
+//  Created by Enver Varshalomidze on 2/13/18.
+//  Copyright © 2018 Enver Varshalomidze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
